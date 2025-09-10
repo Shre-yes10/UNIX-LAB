950e038321f1dd9517cd1c7c4d0092f6db5fb112
Shreyas <sh117sh1178@gmail.com>
2025-09-10 18:29:23 +0000

    Added

---
+++ p9.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Enter Code : "
+read code
+echo "Enter Descriptor : "
+read desc
+
+echo
+if [[ $code=~[0-9]+$ ]]
+then
+	if [[ $desc=~^[a-zA-Z]+$ ]]
+	then
+		echo "$code $desc added successfully"
+		echo $code $desc | cat>>desig.lst
+	else
+		echo "$desc is not an alphabet string"
+	fi
+else
+	echo "$code is not a number"
+fi
+
+echo
+echo "Contents of desig.lst : "
+cat desig.lst
