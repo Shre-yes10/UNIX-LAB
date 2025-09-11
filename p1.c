adefe0d0fac68f38e8f1f2347d3fb0eb559a36d8
Shreyas <sh117sh1178@gmail.com>
2025-09-11 07:12:27 +0000

    Minor Changes

---
+++ p1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	if(argc!=2)
+	{
+		printf("USAGE : command(./a.out) filename(example.txt)\n");
+		exit(1);
+	}
+
+	char buffer[100];
+	FILE *fp;
+	fp=fopen(argv[1],"r");
+
+	if(fp==NULL)
+	{
+		printf("File Doesn't Exists\n");
+		exit(1);
+	}
+
+	while(fgets(buffer,100,fp))
+	{
+		printf("%s",buffer);
+	}
+	fclose(fp);
+
+	return 0;
+}
